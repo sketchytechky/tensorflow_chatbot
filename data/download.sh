cac339bf4f96b62b3370ecaf97ac4e54c28019e3
sketchytechky <boris.w.lau@gmail.com>
2017-09-25 00:17:21 -0400

    Updated download.sh

--- data/download.sh
+++ data/download.sh
@@ -1,3 +1,8 @@
 wget http://www.mpi-sws.org/~cristian/data/cornell_movie_dialogs_corpus.zip
 # unzip to current folder
 unzip -j cornell_movie_dialogs_corpus.zip
+# convert from ISO_8859-1
+for i in `ls *.txt`; do
+    iconv -f 'ISO_8859-1' -t 'utf-8' $i -o $i.utf8
+    mv $i.utf8 $i
+done
